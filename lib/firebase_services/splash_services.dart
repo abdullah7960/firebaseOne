a36f74e6527dfe0c73b95d50365c6823612cb1ea
Abdullah <abdullahabbasi661@gmail.com>
2023-07-05 16:40:09 +0500

    SignOut Feature Success

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -1,12 +1,23 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_one/ui/auth/login_screen.dart';
+import 'package:firebase_one/ui/posts/post_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashServices{
   void isLogin(BuildContext context){
-    Timer(const Duration(seconds: 3),
+    final auth = FirebaseAuth.instance;
+
+    final user = auth.currentUser;
+    if(user != null){
+      Timer(const Duration(seconds: 3),
+     ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>PostScreen()))
+     );
+    }else{
+      Timer(const Duration(seconds: 3),
      ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()))
      );
+    }
   }
 }
